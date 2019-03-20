ae6c748716e02223b7fcc48b14544f4284001ebb
William Merfalen <dev@bnull.net>
2019-03-20 05:34:06 +0000

    auto login functionality. missing cpp/hpp files

---
+++ src/mods/auto-login.cpp
@@ -0,0 +1,7 @@
+#include "auto-login.hpp"
+namespace mods::auto_login {
+	void set_user(std::string_view user){ mods::auto_login::user = user.data(); }
+	const std::string& get_user() { return mods::auto_login::user; }
+	void set_password(std::string_view pass){ mods::auto_login::password = pass.data(); }
+	const std::string& get_password() { return mods::auto_login::password; }
+};
