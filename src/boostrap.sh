f5a3d00a4fde7cae1bd64580da9965a73ae8cb4f
x <xkold@tacticaldenial>
2020-11-20 17:54:04 -0700

    bootstrap.sh for devuan installs

---
+++ src/boostrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DIR=$(pwd)
+sudo apt-get update
+sudo apt-get -y install cmake libssl-dev libpqxx-dev
+mkdir build
+rm -rf yaml-cpp
+git clone 'https://github.com/jbeder/yaml-cpp'
+chmod +x boost-fetcher.sh
+./boost-fetcher.sh
+cd $DIR
+ln -s boost_1_72_0 boost
