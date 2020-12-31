efd43f38cadd9079111d3c002106402a7b4b0fa2
dev <dev@bnull.net>
2020-12-31 14:16:12 -0700

    codestyle fix

--- src/mods/classes/sniper.cpp
+++ src/mods/classes/sniper.cpp
@@ -6,17 +6,17 @@
 #include "../examine.hpp"
 
 namespace mods::classes {
-	sniper::sniper(){
+	sniper::sniper() {
 		this->init();
 	}
-	sniper::sniper(player_ptr_t p){
+	sniper::sniper(player_ptr_t p) {
 		this->init();
 		load_by_player(p);
 	}
-	player_ptr_t 	sniper::player(){
+	player_ptr_t 	sniper::player() {
 		return m_player;
 	}
-	void sniper::init(){
+	void sniper::init() {
 		m_target = 0;
 		m_engaged = 0;
 		m_xray_shot_charges = 0;
@@ -25,37 +25,37 @@ namespace mods::classes {
 	}
 	void sniper::use_claymore(uuid_t object_uuid) {
 		auto it = std::find(m_claymore_instances.begin(),m_claymore_instances.end(),object_uuid);
-		if(it != m_claymore_instances.end()){
+		if(it != m_claymore_instances.end()) {
 			m_claymore_instances.erase(it);
 		}
 	}
-	
-	uint8_t sniper::claymore_count() const{
+
+	uint8_t sniper::claymore_count() const {
 		return m_claymore_instances.size();
 	}
-	void sniper::replenish(){
+	void sniper::replenish() {
 		static uint16_t call_count = 0;
 		++call_count;
 		auto tier = tier(m_player);
-		if((call_count % SNIPER_REPLENISH_PULSE() / tier) == 0){
-			if(m_claymore_instances.size() < SNIPER_CLAYMORE_MAX_COUNT() * tier){
+		if((call_count % SNIPER_REPLENISH_PULSE() / tier) == 0) {
+			if(m_claymore_instances.size() < SNIPER_CLAYMORE_MAX_COUNT() * tier) {
 				auto fatal = create_object(ITEM_EXPLOSIVE,"claymore-mine.yml");
 				m_claymore_instances.emplace_back(fatal->uuid);
 				m_player->carry(fatal);
 				m_player->sendln("{grn}A sniper class claymore mine has been regenerated.{/grn}");
 			}
-			if(m_xray_shot_charges < SNIPER_XRAY_SHOT_MAX_COUNT() * tier){
+			if(m_xray_shot_charges < SNIPER_XRAY_SHOT_MAX_COUNT() * tier) {
 				m_player->sendln("{grn}A sniper class X-Ray shot charge has been regenerated.{/grn}");
 				++m_xray_shot_charges;
 			}
-			#ifdef __MENTOC_SEND_SNIPER_PLAYER_REPLENISH_DEBUG_MESSAGE__
+#ifdef __MENTOC_SEND_SNIPER_PLAYER_REPLENISH_DEBUG_MESSAGE__
 			m_player->sendln("Replenish");
-			#endif
+#endif
 		}
 	}
-	int16_t sniper::new_player(player_ptr_t &player){
+	int16_t sniper::new_player(player_ptr_t& player) {
 		auto db_id = m_orm.initialize_row(player);
-		if(db_id == 0){
+		if(db_id == 0) {
 			report(CAT("unable to initialize row for player (sniper::new_player) ",(player->db_id()),".. player's database id is zero!"));
 			return -2;
 		}
@@ -63,11 +63,11 @@ namespace mods::classes {
 		load_by_player(player);
 		return 0;
 	}
-	int16_t sniper::load_by_player(player_ptr_t & player){
+	int16_t sniper::load_by_player(player_ptr_t& player) {
 		m_player = player;
 		auto result = m_orm.load_by_player(player->db_id());
-		if(result < 0){
-			report(CAT("unable to load sniper class by player id: " , player->db_id() , ".. return status: " , result));
+		if(result < 0) {
+			report(CAT("unable to load sniper class by player id: ", player->db_id(), ".. return status: ", result));
 			return -100 - result;
 		}
 		obj_ptr_t primary = nullptr;
@@ -87,100 +87,100 @@ namespace mods::classes {
 		- Can snipe an enemy through walls or doors
 		- If enemy not behind cover, it causes 150% damage to target
 		*/
-	std::tuple<bool,std::string> sniper::xray_shot(){
+	std::tuple<bool,std::string> sniper::xray_shot() {
 		auto weapon = m_player->primary();
-		if(!weapon || weapon->rifle()->attributes->type != mw_rifle::SNIPER){
+		if(!weapon || weapon->rifle()->attributes->type != mw_rifle::SNIPER) {
 			return {0,"You must be wielding a sniper rifle!"};
 		}
-		if(!m_engaged){
+		if(!m_engaged) {
 			return {0,"You must first use the 'engage' command"};
 		}
 		int distance = weapon->rifle()->attributes->critical_range;
 		int direction = NORTH;
-		if(m_target == 0){
+		if(m_target == 0) {
 			return {0,"You have not marked anyone"};
 		}
 		auto victim = ptr_by_uuid(m_target);
-		if(!victim || victim->visibility() == 0){
+		if(!victim || victim->visibility() == 0) {
 			return {0,"Cannot find your victim!"};
 		}
 		auto feedback = mods::weapons::damage_types::rifle_attack_with_feedback(
-			m_player,
-			weapon,
-			victim,
-			distance,
-			direction
-		);
+		                    m_player,
+		                    weapon,
+		                    victim,
+		                    distance,
+		                    direction
+		                );
 		return {1,"stub"};
 	}
-	void sniper::target_died(uuid_t target){
+	void sniper::target_died(uuid_t target) {
 		m_target = 0;
 		m_engaged = 0;
 		m_player->sendln("Your target died.");
 	}
-	std::tuple<bool,std::string> sniper::mark_target(std::string_view target){
+	std::tuple<bool,std::string> sniper::mark_target(std::string_view target) {
 		m_target = mods::examine::find_player_by_name(m_player, target);
-		if(m_target == 0){
+		if(m_target == 0) {
 			return {0,"Couldn't find a target that matches that string."};
 		}
 		return {1,"Marked target"};
 	}
 
-	std::tuple<bool,std::string> sniper::engage(){
-		if(m_target == 0){
+	std::tuple<bool,std::string> sniper::engage() {
+		if(m_target == 0) {
 			return {0,"You have not marked a target yet!"};
 		}
 		auto ptr = ptr_by_uuid(m_target);
-		if(!ptr){
+		if(!ptr) {
 			m_target = 0;
 			return {0,"That target doesn't exist anymore."};
 		}
 		m_engaged = 1;
 		return {1,CAT("Engaged target: '",ptr->name().c_str(),"'")};
 	}
-	std::tuple<bool,std::string> sniper::disengage(){
+	std::tuple<bool,std::string> sniper::disengage() {
 		m_target = 0;
 		m_engaged = 0;
 		return {1,"You disengage with your target."};
 	}
 
-	std::shared_ptr<sniper> create_sniper(player_ptr_t &in_player){
+	std::shared_ptr<sniper> create_sniper(player_ptr_t& in_player) {
 		return std::move(std::make_shared<sniper>(in_player));
 	}
 };
 
 namespace mods::class_abilities::sniper {
-	ACMD(do_xray_shot){
+	ACMD(do_xray_shot) {
 		PLAYER_CAN("sniper.xray_shot");
 		DO_HELP("xray_shot");
 		auto status = player->sniper()->xray_shot();
 		player->sendln(std::get<1>(status));
 	};
-	ACMD(do_mark_target){
+	ACMD(do_mark_target) {
 		PLAYER_CAN("sniper.mark_target");
 		DO_HELP("mark_target");
 		static constexpr const char* usage = "Usage: mark_target <target>";
 		auto vec_args = PARSE_ARGS();
-		if(vec_args.size() == 0){
+		if(vec_args.size() == 0) {
 			player->sendln(usage);
 			return;
 		}
 		auto status = player->sniper()->mark_target(vec_args[0]);
 		player->sendln(std::get<1>(status));
 	};
-	ACMD(do_engage){
+	ACMD(do_engage) {
 		PLAYER_CAN("sniper.engage");
 		DO_HELP("engage");
 		auto status = player->sniper()->engage();
 		player->sendln(std::get<1>(status));
 	};
-	ACMD(do_disengage){
+	ACMD(do_disengage) {
 		PLAYER_CAN("sniper.disengage");
 		DO_HELP("disengage");
 		auto status = player->sniper()->disengage();
 		player->sendln(std::get<1>(status));
 	};
-	void init(){
+	void init() {
 		mods::interpreter::add_command("mark", POS_RESTING, do_mark_target, 0,0);
 		mods::interpreter::add_command("mark_target", POS_RESTING, do_mark_target, 0,0);
 		mods::interpreter::add_command("engage", POS_RESTING, do_engage, 0,0);
--- src/mods/classes/sniper.hpp
+++ src/mods/classes/sniper.hpp
@@ -8,38 +8,41 @@ using sniper_orm_t = mods::orm::sniper;
 
 namespace mods::classes {
 	struct sniper : base {
-		types kind() {
-			return types::SNIPER;
-		}
-		using primary_choice_t = mods::weapon::sniper::primary_choice_t;
-		using secondary_choice_t = mods::weapon::sniper::secondary_choice_t;
+			types kind() {
+				return types::SNIPER;
+			}
+			using primary_choice_t = mods::weapon::sniper::primary_choice_t;
+			using secondary_choice_t = mods::weapon::sniper::secondary_choice_t;
 
-		/* constructors and destructors */
-		sniper();
-		sniper(player_ptr_t);
-		~sniper() = default;
+			/* constructors and destructors */
+			sniper();
+			sniper(player_ptr_t);
+			~sniper() = default;
 
-		int16_t load_by_player(player_ptr_t &);
-		player_ptr_t player();
-		int16_t new_player(player_ptr_t&);
-		int16_t save();
-		void init();
-		void replenish();
+			int16_t load_by_player(player_ptr_t&);
+			player_ptr_t player();
+			int16_t new_player(player_ptr_t&);
+			int16_t save();
+			void init();
+			void replenish();
 
-		void target_died(uuid_t);
-		std::tuple<bool,std::string> mark_target(std::string_view target);
-		std::tuple<bool,std::string> engage();
-		std::tuple<bool,std::string> disengage();
+			void target_died(uuid_t);
+			std::tuple<bool,std::string> mark_target(std::string_view target);
+			std::tuple<bool,std::string> engage();
+			std::tuple<bool,std::string> disengage();
 
-		/*
-		- Ability: X-Ray Shot (can shoot through multiple layers of walls/doors)
-			- Can snipe an enemy within a building
-			- Can snipe an enemy through walls or doors
-			- If enemy not behind cover, it causes 150% damage to target
-			*/
-		std::tuple<bool,std::string> xray_shot();
-		uint8_t claymore_count() const;
-		void use_claymore(uuid_t);
+
+			std::tuple<bool,std::string> tracking_shot(std::string_view target, direction_t direction);
+
+			/*
+			- Ability: X-Ray Shot (can shoot through multiple layers of walls/doors)
+				- Can snipe an enemy within a building
+				- Can snipe an enemy through walls or doors
+				- If enemy not behind cover, it causes 150% damage to target
+				*/
+			std::tuple<bool,std::string> xray_shot();
+			uint8_t claymore_count() const;
+			void use_claymore(uuid_t);
 
 		private:
 			uuid_t m_target;
@@ -51,7 +54,7 @@ namespace mods::classes {
 			sniper_orm_t m_orm;
 			std::vector<uuid_t> m_scanned;
 	};
-	std::shared_ptr<mods::classes::sniper> create_sniper(player_ptr_t &player);
+	std::shared_ptr<mods::classes::sniper> create_sniper(player_ptr_t& player);
 };
 
 #endif
