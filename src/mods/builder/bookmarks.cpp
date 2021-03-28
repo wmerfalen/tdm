331a760dc01eb51f31f48248c2de18b7a747d937
dev <dev@bnull.net>
2021-03-28 00:47:03 -0600

    bookmark <nickname> <room-id|this>. goto <nickname>

--- src/mods/builder/bookmarks.cpp
+++ src/mods/builder/bookmarks.cpp
@@ -16,16 +16,20 @@ namespace mods::builder::bookmarks {
 		if(!player->builder_data) {
 			mods::builder::initialize_builder(player);
 		}
-		if(vec_args.size() < 3) {
-			player->errorln("Usage: bookmark nickname room-id");
+		player->sendln(CAT("size: '",vec_args.size(),"'"));
+		if(vec_args.size() < 2) {
+			player->errorln("Usage: bookmark nickname <room-id|this>");
 			return;
 		}
-		auto i = mods::util::stoi(vec_args[2]).value_or(-1);
-		if(i < 0) {
-			player->errorln(CAT("Invalid room id: ",vec_args[2],". Skipping...").c_str());
-			return;
+		int i = player->room();
+		if(vec_args[1].compare("this") != 0) {
+			i = mods::util::stoi(vec_args[1]).value_or(-1);
+			if(i < 0) {
+				player->errorln(CAT("Invalid room id: ",vec_args[1],". Skipping...").c_str());
+				return;
+			}
 		}
-		player->builder_data->bookmarks[vec_args[1]] = i;
+		player->builder_data->bookmarks[vec_args[0]] = i;
 		ADMIN_DONE();
 	}
 	ACMD(do_goto) {
@@ -35,11 +39,20 @@ namespace mods::builder::bookmarks {
 		if(!player->builder_data) {
 			mods::builder::initialize_builder(player);
 		}
-		auto room_id = player->builder_data->bookmarks[vec_args[1]];
+		if(vec_args.size() < 1) {
+			player->errorln("You must specify a nickname");
+			return;
+		}
+		auto room_id = player->builder_data->bookmarks[vec_args[0]];
 		if(room_id >= world.size()) {
 			player->errorln(CAT("Room id out of bounds. You asked for: ",room_id, " but world.size is:", world.size()));
 			return;
 		}
+		if(room_id < 0) {
+			player->errorln("Room id is a negative number. Ignoring...");
+			return;
+		}
+
 		char_from_room(player->cd());
 		char_to_room(player->cd(),room_id);
 	}
