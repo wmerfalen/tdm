0185c3e5bdf1693bf3876cc54c7bbd9a02314dad
William Merfalen <wmerfalen@gmail.com>
2021-10-02 16:18:01 -0600

    tt-wrapper can work on multiple workstations now

--- src/scripts/tt-wrapper.sh
+++ src/scripts/tt-wrapper.sh
@@ -3,7 +3,7 @@
 TD_CLASS="$1"
 
 while [[ 1 ]]; do
-	cd /home/xkold/cmpp/bin/tintin-scripts
+	cd $WEBROOT/scripts/tintin-scripts
 	rlwrap -a -H $PWD/histories/${TD_CLASS} ~/bin/tt++ -r ./${TD_CLASS}.tt -e 'start'
   sleep 1
 done
